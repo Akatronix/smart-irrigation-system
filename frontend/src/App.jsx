57a13be31ae91bb3beb939c760d69a3896157806
Akatronix <153951875+Akatronix@users.noreply.github.com>
2024-10-31 00:46:42 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import logo from "/logo.jpg";
-import PlantBoxing from "./components/plantBox";
+// import PlantBoxing from "./components/plantBox";
 import { FaTemperatureLow } from "react-icons/fa";
 import { GiWaterTank } from "react-icons/gi";
 import { useEffect, useState } from "react";
@@ -102,5 +102,26 @@ function App() {
     </div>
   );
 }
+
+
+
+const PlantBoxing = ({ name, value, state }) => {
+  return (
+    <div className="w-[330px] h-full p-[20px] bg-white rounded-md mb-[20px]">
+      <PiPlantFill className="w-[30px] h-[30px] text-green-400" />
+      <p>{name}</p>
+      <p>
+        <span className="text-gray-500">Value:</span>
+        <span className="text-2xl md:text-3xl font-bold ml-[10px]">
+          {value}
+        </span>
+      </p>
+      <p>
+        <span className="text-gray-500">Tap State:</span>
+        <span className="ml-[10px]">{state}</span>
+      </p>
+    </div>
+  );
+};
 
 export default App;
